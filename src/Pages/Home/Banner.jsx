8fec737c94ecbf36b974b99296e492b16e3fe9c9
borhan98 <borhanuddindns420@gmail.com>
2024-01-23 01:55:19 +0600

    banner section added

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,27 @@
+import { HiOutlineArrowLongDown } from "react-icons/hi2";
+
+
+const Banner = () => {
+    return (
+        <div className="hero h-[100vh]" style={{ backgroundImage: 'url(https://i.ibb.co/D4dLZhM/banner-bg2.jpg)' }}>
+            <div className="hero-overlay bg-opacity-90"></div>
+            <div className="text-center text-neutral-content">
+                <div className="max-w-md">
+                    <p className="mb-5 text-xl font-bold">Find Your</p>
+                    <h1 className="mb-5 text-6xl">
+                        Dream Home
+                        <span className="text-xl"> with </span>
+                        <span className="text-[#F89A20]">HouseHunter</span>
+                    </h1>
+                    <p>
+                        Empowering Your Home Search – HouseHunter, Where Choices Abound
+                    </p>
+                    <a href="#" className="scroll-arrow inline-block"><HiOutlineArrowLongDown className="text-5xl" /></a>
+                    <p className="mt-6 tracking-widest">Scroll</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Shared/Navbar.jsx
@@ -0,0 +1,65 @@
+import { Link, NavLink } from "react-router-dom";
+import HouseHunterLogo from "../assets/HouseHunterLogo2.png";
+
+
+const Navbar = () => {
+
+    const activeRoute = ({ isActive }) =>
+        isActive
+            ? "bg-[#F89A20] py-2 px-3 rounded-md text-white uppercase"
+            : "py-2 px-3 rounded-md uppercase text-white";
+
+    const menuLinks = (
+        <>
+            <li>
+                <NavLink className={activeRoute} to={"/"}>
+                    Home
+                </NavLink>
+            </li>
+            <li>
+                <NavLink className={activeRoute} to={"/signup"}>
+                    sign up
+                </NavLink>
+            </li>
+        </>
+    );
+
+    return (
+        <div className=" fixed w-full shadow-md">
+            <div className="container mx-auto px-2 lg:px-0 navbar py-4">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuLinks}
+                        </ul>
+                    </div>
+                    <div>
+                        {/* Website Logo */}
+                        <Link>
+                            <figure>
+                                <img className="w-7 md:w-9 lg:w-40" src={HouseHunterLogo} alt="Logo" />
+                            </figure>
+                        </Link>
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        {menuLinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <Link to={"/login"}>
+                        <button className="py-2 px-4 font-medium rounded-md bg-[#F89A20] text-white uppercase">
+                            sign in
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
